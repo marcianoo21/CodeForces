7f4f6ba07b49899da2c23f77c3142a609b79d5e5
killu21 <mgrzelak218@gmail.com>
2024-06-04 10:36:32 +0200

    task900

---
+++ Businesstrip900.py
@@ -0,0 +1,17 @@
+k = int(input())
+x = 0
+z = 0
+a = list(map(int, input().split()))
+ai = sorted(a)
+if sum(a) < k:
+    print("-1")
+
+elif k > 0:     
+    while x < k:
+        x += max(ai)
+        ai.pop()
+        z += 1
+    print(z)
+    
+else:
+    print(0)
