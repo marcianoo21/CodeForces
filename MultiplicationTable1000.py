e4092b4e2979acbc91a118f6e9424099d3627f05
killu21 <mgrzelak218@gmail.com>
2024-06-16 23:41:04 +0200

    a

---
+++ MultiplicationTable1000.py
@@ -0,0 +1,10 @@
+n, x = map(int, input().split())
+dividers = []
+
+for i in range(1, n+1):
+    if x % i == 0 and x / i <= n:
+        dividers.append(i)
+
+
+# print(f"n: {n}, x: {x}")
+print(len(dividers))
