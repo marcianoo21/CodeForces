123d158bd848c4d3e51a556486b29214245f0dfb
killu21 <mgrzelak218@gmail.com>
2024-06-16 23:40:53 +0200

    f

---
+++ Maximums900.py
@@ -0,0 +1,20 @@
+n = int(input())
+b = list(map(int, input().split()))
+a = []
+rest = []
+x = 1
+a.append(b[0])
+rest.append(b[0])
+
+while x < n:
+    if b[x-1] >= 0:
+        rest.append(b[x])       
+    else:   
+        rest.pop()
+        rest.append(b[x])
+
+    a.append(sum(rest))
+    x += 1
+
+print(*a)
+# jebane time complexity za duze://///
