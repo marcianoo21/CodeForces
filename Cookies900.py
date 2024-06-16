aecef9be607c5fc99802605e5fe1edbc5178dc2e
killu21 <mgrzelak218@gmail.com>
2024-06-16 23:41:42 +0200

    cleaned

--- Cookies900.py
+++ Cookies900.py
@@ -9,27 +9,3 @@ for x in a:
     else:
         continue
 print(counter)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#     if x % 2 != 0:
-#         counter += 1
-# if counter == 0:
-#     print(0)
-# elif counter % 2 != 0:
-#     print(counter)
-# elif sum(a) 
-# else:
-#     print(len(a) - counter)
