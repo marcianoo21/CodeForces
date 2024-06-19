9bfc6aa3979a15681add2b03171b902b73b95c24
killu21 <mgrzelak218@gmail.com>
2024-06-19 22:56:52 +0200

    cf2

---
+++ NastyaandRice900.py
@@ -0,0 +1,12 @@
+t = int(input())
+
+for _ in range(t):
+    n, a, b, c, d = map(int, input().split())
+    min_rices = n * (a-b)
+    max_rices = n * (a+b)
+    min_pack = c - d
+    max_pack = c + d
+    if min_rices > max_pack or max_rices < min_pack:
+        print("NO")
+    else:
+        print("YES")
