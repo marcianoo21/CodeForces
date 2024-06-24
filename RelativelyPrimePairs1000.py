fb4e406a2efc00a6e744f01650950f72bfa2d315
killu21 <mgrzelak218@gmail.com>
2024-06-24 22:34:50 +0200

    1000

---
+++ RelativelyPrimePairs1000.py
@@ -0,0 +1,10 @@
+l, r = map(int, input().split())
+
+solutions_ammount = (r-l+1) // 2
+
+if (l >= r) or ((r - l) % 2 != 1):
+    print("NO")
+else:
+    print("YES")
+    for i in range(solutions_ammount):
+       print(l+i*2, " ", l+i*2+1) #adjacent numbers will always have a greatest common equal to 1;)
