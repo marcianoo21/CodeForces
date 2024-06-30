fe680c4a49bcd6d9879f889053d07e51300771c4
killu21 <mgrzelak218@gmail.com>
2024-06-30 22:40:47 +0200

    a

--- UniqueNumber900.py
+++ UniqueNumber900.py
@@ -1,45 +1,3 @@
-# t = int(input())
-
-# # max 45
-# # sum of digits equal to x and all digits are distinct
-# for _ in range(t):
-#     x = int(input())
-#     if x < 10:
-#         print(x)
-#     elif x > 45:
-#         print(-1)
-#     elif x == 45:
-#         print(123456789)
-#     elif x >= 35 and x <= 45:
-#         for a in range(56789, 23456790, 1000):
-#             nlis = [int(digit) for digit in str(a)]
-#             if len(nlis) != len(set(nlis)):
-#                 continue
-#             if sum(nlis) == x:
-#                 break
-#         nlis.sort()                
-#         print("".join(map(str, nlis)))
-#     elif x >= 25 and x <= 34:
-#         for a in range(1789, 56789, 100):
-#             nlis = [int(digit) for digit in str(a)]
-#             if len(nlis) != len(set(nlis)):
-#                 continue
-#             if sum(nlis) == x:
-#                 break     
-#         nlis.sort()           
-#         print("".join(map(str, nlis)))
-#     else:
-#         for a in range(19, 790, 10):
-#             nlis = [int(digit) for digit in str(a)]
-#             if len(nlis) != len(set(nlis)):
-#                 continue
-#             if sum(nlis) == x:
-#                 break         
-#         nlis.sort()     
-#         print("".join(map(str, nlis)))
-                    
-        
-        
 t = int(input())
 for _ in range(t):
     n = int(input())
@@ -48,7 +6,7 @@ for _ in range(t):
         if n >= i:
             s += str(i)
             n -= i
-    s = s[::-1]  # Reverse the string
+    s = s[::-1]  
     if n != 0:
         s = "-1"
     print(s)
