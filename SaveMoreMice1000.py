b7c9aec1ac1a5840341198ddfa4d7f7e8dcbd7e8
killu21 <mgrzelak218@gmail.com>
2024-06-30 22:39:41 +0200

    mice

---
+++ SaveMoreMice1000.py
@@ -0,0 +1,19 @@
+def main():
+    t = int(input())
+    
+    for _ in range(t):
+        n, k = map(int, input().split())
+        x = list(map(int, input().split()))
+        x.sort(reverse=True)
+        
+        cnt = 0
+        sum_val = 0
+        
+        while cnt < len(x) and sum_val + n - x[cnt] < n:
+            sum_val += n - x[cnt]
+            cnt += 1
+        
+        print(cnt)
+
+if __name__ == "__main__":
+    main()
