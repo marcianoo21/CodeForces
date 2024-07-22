6583cd7b88e8505d5b76f5cd6381a8a6a6532f2e
killu21 <mgrzelak218@gmail.com>
2024-07-22 22:54:24 +0200

    cleaning

--- Lazyland900.py
+++ Lazyland900.py
@@ -6,9 +6,3 @@ val = 0
 diff = k - len(set(a))
 if len(a) == len(set(a)):
     print(0)
-# else:
-#     for x in range(len(a)):
-#         a = set(a)
-#         for y in range(len(b)):
-#             if min < b[y]:
-#                 min = b[y]
