1cf836c17f9f27e84416642ff6e79adfca0b7b12
killu21 <mgrzelak218@gmail.com>
2024-07-22 22:56:15 +0200

    cleaning

--- RelativelyPrimePairs1000.py
+++ RelativelyPrimePairs1000.py
@@ -7,4 +7,4 @@ if (l >= r) or ((r - l) % 2 != 1):
 else:
     print("YES")
     for i in range(solutions_ammount):
-       print(l+i*2, " ", l+i*2+1) #adjacent numbers will always have a greatest common equal to 1;)
+       print(l+i*2, " ", l+i*2+1) 
