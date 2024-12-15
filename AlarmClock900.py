c7fc03bac4d40397dcff7dd45c9851bbeee10ec5
killu21 <mgrzelak218@gmail.com>
2024-12-16 00:40:52 +0100

    working properly

--- AlarmClock900.py
+++ AlarmClock900.py
@@ -1,22 +1,22 @@
+import math
+
 t = int(input())
 
-times = []
+result = 0
 
-for _ in range(t):
-    a, b, c ,d = map(int, input().split())
+for _ in range(t): 
+    a, b, c ,d = map(int, input().split())  
+    
+    if b < a and c <= d:
+        print(-1)
+    elif b >= a:
+        print(b)
+    else:
+        diff = c - d
+        snooze = math.ceil((a-b) / diff)  
+        if snooze < 0:
+            snooze = 0
+        result = b + (snooze * c)
+        print(result)
 
-    if b < a and c < d:
-        print("-1")
-        continue
-    elif a == b:
-        print(a)
-        continue
-    res_time = a - b
-    diff = c - d
-    while res_time > 0:
-        res_time -= diff
-        b += c 
-    print(b)
 
-
-# time exceeded
