baf2e8d54f6ca5813dbeead68356cdbcd1d96905
killu21 <mgrzelak218@gmail.com>
2025-01-01 19:14:25 +0100

    teraz dziala

--- Letters1000.py
+++ Letters1000.py
@@ -2,7 +2,6 @@ n, m = map(int, input().split())
 a = list(map(int, input().split()))
 b = list(map(int, input().split()))
 pivot = 0
-v = 0
 result = []
 a_pivots = []
 room_number = []
@@ -16,14 +15,10 @@ for num1 in b:
     while display < len(a_pivots) and num1 > a_pivots[display]:
         display += 1
     result.append(display + 1)
-
-for j in b:
-    if v + 1 < len(a_pivots) and j > a_pivots[v + 1]:
-        v += 1
-    if j > a_pivots[v]: 
-        room_number.append(j - a_pivots[v])
+    if display == 0:
+        room_number.append(num1)
     else:
-        room_number.append(j)
+        room_number.append(num1 - a_pivots[display - 1])
 
 for i in range(len(result)):
     print(result[i], room_number[i])
