e7dd98ebc6739a03df0129241665a7f49db41b73
killu21 <mgrzelak218@gmail.com>
2025-01-05 00:57:55 +0100

    zadanko

---
+++ Chatroom1000.py
@@ -0,0 +1,16 @@
+
+s = str(input())
+word = 'hello'
+i = 0
+j = 0
+
+
+while i < len(s) and j < len(word):
+    if s[i] == word[j]:
+        j += 1
+    i += 1 
+
+if j == len(word):
+    print("YES")
+else:
+    print("NO")
