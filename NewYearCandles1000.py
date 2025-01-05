d4022ac882c949cf8668284536b0178a9f37cd55
killu21 <mgrzelak218@gmail.com>
2025-01-05 01:40:29 +0100

    easy task

---
+++ NewYearCandles1000.py
@@ -0,0 +1,16 @@
+a, b = map(int, input().split())
+sum = 0
+lefts = 0
+
+if b > a:
+    print(a)
+else:
+    sum += a
+    while a >= b: 
+        lefts = 0
+        lefts = a % b     
+        a = (a // b) 
+        sum += a
+        a += lefts  
+       
+    print(sum)
