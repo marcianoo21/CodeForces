3c594297eb82a5ddcdf97bf7ba7f141c3255d9b7
killu21 <mgrzelak218@gmail.com>
2025-01-18 03:22:35 +0100

    comp

--- competition/Taxi1100.py
+++ competition/Taxi1100.py
@@ -11,8 +11,6 @@
 
 
 
-
-
 ### Rozwiązanie gdy nie można zmieniać kolejności 
 
 # n = int(input())
@@ -41,26 +39,21 @@
 n = int(input())
 s = list(map(int, input().split()))
 
-# Liczba grup o rozmiarze 1, 2, 3 i 4
 count1 = s.count(1)
 count2 = s.count(2)
 count3 = s.count(3)
 count4 = s.count(4)
 
-# Każda grupa o rozmiarze 4 potrzebuje osobnej taksówki
 result = count4
 
-# Grupy o rozmiarze 3 mogą być połączone z grupami o rozmiarze 1
 result += count3
 count1 = max(0, count1 - count3)
 
-# Dwie grupy o rozmiarze 2 mogą dzielić jedną taksówkę
 result += count2 // 2
 if count2 % 2 != 0:
     result += 1
     count1 = max(0, count1 - 2)
 
-# Pozostałe grupy o rozmiarze 1 mogą być połączone w taksówki
 result += count1 // 4
 if count1 % 4 != 0:
     result += 1
