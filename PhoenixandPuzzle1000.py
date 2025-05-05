faa66760de3f7bcb3e1cedff68a642306be5cf7b
marcianoo21 <mgrzelak218@gmail.com>
2025-05-05 17:15:56 +0200

    a

--- PhoenixandPuzzle1000.py
+++ PhoenixandPuzzle1000.py
@@ -2,7 +2,6 @@ t = int(input())
 
 for _ in range(t):
     n = int(input())
-    
     if (n / 2)**0.5 == round((n / 2)**0.5) or (n / 4)**0.5 == round((n / 4)**0.5) or n == 2 or n == 4:    
         print("YES")
     else:
