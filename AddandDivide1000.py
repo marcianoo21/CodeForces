47efef472bbb9519d9457e3a3fee1b2c2738a4ee
marcianoo21 <mgrzelak218@gmail.com>
2025-05-06 23:33:56 +0200

    working and optimized version;)))

--- AddandDivide1000.py
+++ AddandDivide1000.py
@@ -1,14 +1,9 @@
-import math
 
-def dividions_number(a, b):
+def divisions_number(a, b):
     count = 0
-    while a > b:
-        a = math.floor(a / b)
+    while a > 0:
+        a //= b
         count += 1
-    if a == b:
-        count += 2
-    else:
-        count += 1  
     return count
 
 t = int(input())
@@ -21,11 +16,10 @@ for _ in range(t):
         b_increm = 1
         b += b_increm
    
-    max_val = dividions_number(a, b) + b_increm
-    for i in range(3):
-        temp = dividions_number(a, b+i)
-        if temp + i < max_val:
-            max_val = temp + i
-
+    max_val = divisions_number(a, b) + b_increm
+    for i in range(9):
+        temp = divisions_number(a, b+i)
+        max_val = min(max_val, temp + i)
+            
     print(max_val + b_increm)
     
