b3d7751ede8d2396b0230b14ad2aaf8c978ed4d4
marcianoo21 <mgrzelak218@gmail.com>
2025-05-08 22:48:52 +0200

    s

--- Laptops1100.py
+++ Laptops1100.py
@@ -1,18 +1,21 @@
 n = int(input())
  
 a1 = 0
-a2 = 0
+b1 = 1
 res = ''
 for i in range(n):
     
     a, b = map(int, input().split())
-    if i == 0:
+    if i == 0 or a1 == b1:
         a1, b1 = a, b
     if a < a1 and b > b1 or a > a1 and b < b1:
         res = 'Happy Alex'
         break
     else:
         res = 'Poor Alex'
-        # a1, b1 = a, b
     
 print(res)
+
+
+
+
