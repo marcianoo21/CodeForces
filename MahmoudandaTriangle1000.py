5996ed97e94ba237f5f2454cb296da6bf8c1ec78
marcianoo21 <mgrzelak218@gmail.com>
2025-05-11 00:13:14 +0200

    done

--- MahmoudandaTriangle1000.py
+++ MahmoudandaTriangle1000.py
@@ -2,4 +2,13 @@ n = int(input())
 
 a = list(map(int, input().split()))
 
-print(sorted(a))
+if len(a) < 3:
+    print("NO")
+else:
+    a_sort = sorted(a)
+    for i in range(2, n):
+       if a_sort[i-2] + a_sort[i-1] > a_sort[i]:
+           print("YES")
+           break
+    else:
+        print("NO")
