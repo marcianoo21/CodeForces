a44f150df80cd207f4c1de54b9edb57c9138913f
marcianoo21 <mgrzelak218@gmail.com>
2025-05-20 23:52:41 +0200

    proste hmm

---
+++ Joysticks1100.py
@@ -0,0 +1,28 @@
+a1, a2 = map(int, input().split())
+
+time = 0
+while a1 > 0 and a2 > 0:
+    if a1 + a2 <= 2:
+        break
+    elif a1 <= 2:
+        a1 += 1
+        a2 -= 2
+    elif a2 <= 2:
+        a2 += 1
+        a1 -= 2
+    elif a1 > a2:
+        a2 += 1
+        a1 -= 2
+    elif a1 < a2:
+        a1 += 1
+        a2 -= 2
+    elif a1 + a2 <= 2:
+        time -= 1
+        print("MNIEJ")
+    else:
+        a1 += 1
+        a2 -= 2
+        print("HERE")
+    time += 1
+
+print(time)
