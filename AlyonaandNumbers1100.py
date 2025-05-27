07a3b4265a7e392ceb1921285fe440d510e58e7a
marcianoo21 <mgrzelak218@gmail.com>
2025-05-27 23:35:43 +0200

    w

--- AlyonaandNumbers1100.py
+++ AlyonaandNumbers1100.py
@@ -4,6 +4,7 @@ counter = 0
 x = 1
 y = 4
 iterations = 0
+
 while x <= n:
     iterations += 1
     if y <= m:
@@ -15,10 +16,5 @@ while x <= n:
             y += 5
         x += 1
         iterations = 0
-    
-    
+       
 print(counter)
-     
-# 5 5 - 1-4 2-3 3-2 4-1 (4)
-# 6 12   1-4 1-9 2-3 2-8 3-2 3-7 3-12 4-1 4-6 4-11 5-5 5-10 6-4 6-9  (14)
-#           2        2         3           3           2        2
