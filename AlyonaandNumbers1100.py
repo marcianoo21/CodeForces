4d8b38bdd17811c78c6dad17fa674fd17ea1785c
marcianoo21 <mgrzelak218@gmail.com>
2025-05-27 23:47:52 +0200

    2

--- AlyonaandNumbers1100.py
+++ AlyonaandNumbers1100.py
@@ -18,3 +18,21 @@ while x <= n:
         iterations = 0
        
 print(counter)
+
+### 2nd version(faster)
+def count_pairs(n, m):
+    cnt_n = [0] * 5
+    cnt_m = [0] * 5
+
+    for i in range(5):
+        cnt_n[i] = n // 5 + (1 if i != 0 and i <= n % 5 else 0)
+        cnt_m[i] = m // 5 + (1 if i != 0 and i <= m % 5 else 0)
+
+    result = 0
+    for i in range(5):
+        result += cnt_n[i] * cnt_m[(5 - i) % 5]
+
+    return result
+
+n, m = map(int, input().split())
+print(count_pairs(n, m))
