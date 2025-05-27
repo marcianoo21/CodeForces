83748c7ef06bd39a149df706c399cfcb9dee255d
marcianoo21 <mgrzelak218@gmail.com>
2025-05-27 23:33:28 +0200

    works but to slow;(

--- AlyonaandNumbers1100.py
+++ AlyonaandNumbers1100.py
@@ -3,14 +3,21 @@ n, m = map(int, input().split())
 counter = 0
 x = 1
 y = 4
+iterations = 0
 while x <= n:
-    if y < m:
+    iterations += 1
+    if y <= m:
         counter += 1
+        y += 5
     else:
-        y -= 1
+        y -= (iterations - 1)*5 +1
+        if y < 1:
+            y += 5
         x += 1
-    y += 5
+        iterations = 0
     
+    
+print(counter)
      
 # 5 5 - 1-4 2-3 3-2 4-1 (4)
 # 6 12   1-4 1-9 2-3 2-8 3-2 3-7 3-12 4-1 4-6 4-11 5-5 5-10 6-4 6-9  (14)
